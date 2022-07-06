ab2aa115629c03f9882a118c968bb697d9ef4fee
Guilherme Santos <agustinho_santos@hotmail.com>
2022-07-06 12:06:00 -0300

    filtro genre ok

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::MoviesController < Api::V1::BaseController
 
   def index
-    @movies = Movie.all.order(published_at: :asc)
+    @movies = Movie.all.order(year: :asc)
   end
 
+  def tv_show
+    @movies = Movie.where(genre: "TV Show")
+  end
+
+  def movie
+    @movies = Movie.where(genre: "Movie")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :movies, only: [ :index ]
+      resources :movies, only: [ :index ] do
+        collection do
+          get :tv_show, defaults: { format: :json }
+          get :movie, defaults: { format: :json }
+        end
+      end
     end
   end
 end
