ad54f82b83cbc9aaefea0fc4fe63af7025288b6e
Guilherme Santos <agustinho_santos@hotmail.com>
2022-07-12 13:12:26 -0300

    retirada de caminho root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'movies#index'
+  #root to: 'movies#index'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
